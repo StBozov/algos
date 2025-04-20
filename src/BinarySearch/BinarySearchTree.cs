44c928f8f444109cfd70ef0b4f65185e99b5f2e8
stbozov <stefan.bozov@outlook.com>
2025-04-20 19:50:41 +0300

    refactoring

--- src/BinarySearch/BinarySearchTree.cs
+++ src/BinarySearch/BinarySearchTree.cs
@@ -6,7 +6,11 @@ public class BinarySearchTree(int rootKey) : IEnumerable<int>
 {
     private readonly Node root = new(rootKey);
 
+    public int Count => root.Count;
+
     public bool Insert(int key) => root.Insert(key);
+    public bool Exist(int key) => root.Exist(key);
+    public int? Delete(int key) => root.Delete(key)?.Key;
 
     public IEnumerator<int> GetEnumerator()
     {
